c0285744c2da0cd049c87fc8afe363b177eee4d0
= <j.kurtbayaras@gmail.com>
2022-08-31 20:22:23 +0800

    fix: change code in task form

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -2,35 +2,51 @@
   <h1 class="text-3xl font-bold text-center mt-5 mb-5">Create Project </h1> 
   <div class="form-section">
     <div class="container mx-auto">
-      <form>
+      <form @submit.prevent>
         <h1 class="text-sm uppercase">Title</h1>
-        <input type="text" v-model="form.name" class="mb-5 w-full border border-slate-300 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"/>
+        <input type="text" v-model="form.title" class="mb-5 w-full border border-slate-300 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"/>
         <h1 class="text-sm uppercase">Description</h1>
-        <textarea  class="mb-5 w-full border border-slate-300 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"/>
-        <button class="button w-full rounded py-2" @click.prevent="submitTask">Add Task</button>
+        <textarea v-model="form.description" class="mb-5 w-full border border-slate-300 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"/>
+        <button class="button w-full rounded py-2" @click="submitTask">Add Task</button>
       </form>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import useProjects from "@/composables/Projects";
 import { reactive, ref, computed } from "vue";
+import { useRouter } from 'vue-router'
 import { v4 as uuidv4 } from "uuid";
+const { addProjects } = useProjects();
+const router = useRouter();
 
 interface Task {
-  name: Array<string>
+  name: string,
+  description: string
 }
 const task = ref([] as Task[]);
 const form = reactive({
-  name: ""
+  title: "",
+  description: "",
+  completed: false
 });
 
-const submitTask = () => {
-  if (form.name === undefined) {
+const submitTask = async () => {
+  if (form.title === undefined) {
     return;
   }
-  task.value.push(form.name)
-  form.name = ""
+  try {
+    await addProjects(
+      uuidv4,
+      form.title,
+      form.description,
+      form.completed
+    )
+    router.push("/")
+  } catch (e) {
+    console.log(e)
+  }
 };
 
 const length = computed(() => {
