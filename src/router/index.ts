4d1d0fa640050656a756144d9f739a6cfd6bdddc
= <j.kurtbayaras@gmail.com>
2022-09-04 20:47:55 +0800

    feature: route navigation of login and sign in

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import TaskForm from "@/components/TaskForm.vue";
 import EditProject from "@/components/EditForm.vue";
+import SignIn from "@/views/SignIn.vue";
+import Register from "@/views/Register.vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: "/project",
@@ -19,11 +25,47 @@ const routes: Array<RouteRecordRaw> = [
     name: "editProject",
     component: EditProject
   },
+  {
+    path: "/register",
+    name: "register",
+    component: Register
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: SignIn
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener();
+        resolve(user);
+      },
+      reject
+    )
+  })
+}
+
+router.beforeEach(async (to, from, next) => {
+  if(to.matched.some((record:any) => record.meta.requiresAuth)) {
+    if(await getCurrentUser) {
+      next();
+    } else {
+      next("/login")
+    }
+  } else {
+    next();
+  }
+});
+
 
 export default router;
