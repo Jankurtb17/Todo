8a8d9dbe3771994459ae830330cd9144fb459f43
= <j.kurtbayaras@gmail.com>
2022-09-04 22:03:46 +0800

    refactor: adjust text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,41 @@
 <template>
   <div class="home">
-    <h1 class="text-3xl font-bold text-center mt-5 mb-5">Project List</h1>
-      <div v-if="isLoading">
-        <Loading />
+    <h1 class="text-3xl font-bold sm:text-center md:text-center text-center mt-5 mb-5">Project List</h1>
+    <div v-if="isLoading">
+      <Loading 
+        :item="getLength"
+      />
       </div>
       <div v-else>
         <div v-for="project in projects" :key="project">
           <SingleProject
             :project="project"
             @openDialog="openModal"
+            @deleteProj="deleteProjects"
           />
         </div>
       </div>
-    <transition name="fade">
-      <Teleport to=".modals" v-if="deleteModal">
-        <Modal teleport="body" @closeModal="openModal">
-          <div class="flex px-4 py-4">
-            <span class="material-icons text-red-500 pr-3">warning</span> 
-            <div>
-              <h1 class="font-medium tracking-wide text-xl">Delete Project</h1>
-              <p>Are you sure you want to delete this project ?</p>
-            </div>
-          </div>
-          <div class="flex justify-end bg-gray-100 space-x-2 mt-5 py-2 px-1 rounded">
+
+      <transition name="fade">
+        <Teleport to=".modals" v-if="deleteModal">
+          <Modal teleport="body" 
+            title="Delete Project"
+            message="Are you sure you want to delete this project?"
+            type="warning"
+            @closeModal="openModal"
+          >
             <button class="rounded px-4 bg-white border border-slate-300 py-1" @click="openModal">Cancel</button>
-            <button class="rounded px-4 bg-white border bg-red-700 text-white focus:bg-red-800 border-slate-300 py-1">Delete</button>
-          </div>
-        </Modal>
-      </Teleport>
-    </transition>
+            <button class="rounded px-4 bg-white border bg-red-700 text-white focus:bg-red-800 border-slate-300 py-1" @click="removeProject(deleteProjects)">Delete</button>
+          </Modal>
+        </Teleport>
+      </transition>
+      
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref }  from "vue";
+import { computed, onMounted, onUpdated, ref, watchEffect }  from "vue";
+import AlertMessage from "@/components/AlertMessage.vue";
 import TaskList from "@/components/TaskList.vue";
 import useProjects from "@/composables/Projects.ts";
 import SingleProject from "@/components/SingleProject.vue";
@@ -44,6 +46,7 @@ const projects = ref([] as Projects[]);
 const isLoading = ref(false);
 const deleteModal = ref(false);
 const updateModal = ref(false);
+const delId = ref();
 
 type Projects = {
   id: string,
@@ -64,18 +67,25 @@ const displayProjects = async () => {
   isLoading.value = true;
   projects.value = await getData();
   isLoading.value = false;
+  deleteModal.value = false;
 }
 
-const deleteProjects = async (id: any) => {
+const deleteProjects = (id: any) => {
+  delId.value = id;
+}
+
+const removeProject = async () => {
   try {
-    await deleteProject(id)
-  } catch (e) {
-    if(e === typeof("string"))
-    {
-      
-    }
+    await deleteProject(delId.value)
+    displayProjects()
+  } catch(e) {
+    console.log(e.message)
   }
 }
+
+const getLength = computed(() => {
+  return projects.value.length;
+})
 
 onMounted(() => {
   displayProjects();
@@ -84,12 +94,17 @@ onMounted(() => {
 </script>
 
 <style scoped>
+
+h1 {
+  color: #314cb6;
+}
 .fade-enter-from {
   opacity: 0;
 } 
 
 .fade-enter-active {
-  transition: all 2s ease-in;
+  transition: all .5s ease-in;
+  transform: translateX(30px);
 }
 
 .fade-leave-to {
@@ -97,6 +112,6 @@ onMounted(() => {
 }
 
 .fade-leave-active {
-  transition: all 2s ease-out;
+  transition: all .5s ease-out;
 }
 </style>
