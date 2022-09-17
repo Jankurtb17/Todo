48ca13667cc9004227cdcfbde02a48ebf938f65a
= <j.kurtbayaras@gmail.com>
2022-09-17 19:38:39 +0800

    feature: update status

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,55 @@
 <template>
   <div class="home">
     <h1 class="text-3xl font-bold sm:text-center md:text-center text-center mt-5 mb-5">Project List</h1>
+    
     <div v-if="isLoading">
       <Loading 
         :item="getLength"
       />
-      </div>
-      <div v-else>
-        <div v-for="project in projects" :key="project">
-          <SingleProject
-            :project="project"
-            @openDialog="openModal"
-            @deleteProj="deleteProjects"
-          />
+    </div>
+    <div v-else>
+      <div class="projects-ongoing">
+        <div v-if="projects.length">
+          <div class="ongoing mx-20 sm:mx-2 sm:mb-4">
+            <TransitionGroup name="fade" tag="p">
+              <p class="uppercase text-sm font-bold text-gray-400 tracking-wide">Ongoing</p>
+              <span class="list-items" v-for="project in projects" :key="project.id">
+                <div v-if="!project.completed" class="w-full">
+                  <SingleProject
+                    :project="project"
+                    @openDialog="openModal"
+                    @deleteProj="deleteProjects"
+                    @task="completeTask"
+                  />
+                </div>
+              </span>
+            </TransitionGroup>
+          </div>
+          <div class="completed mx-20 sm:mx-2">
+            <TransitionGroup name="fade" tag="p">
+              <p class="uppercase text-sm font-bold text-gray-400 tracking-wide">Completed</p>
+              <span class="list-items" v-for="project in projects" :key="project.id">
+                <div v-if="project.completed">
+                  <SingleProject
+                    :project="project"
+                    @openDialog="openModal"
+                    @deleteProj="deleteProjects"
+                    @task="completeTask"
+                  />
+                </div>
+              </span>
+            </TransitionGroup>
+          </div>
+        </div>
+        <div v-else>
+          <p class="text-gray-400">Wohooo, nothing left on task</p>
         </div>
       </div>
+    </div>
 
-      <transition name="fade">
-        <Teleport to=".modals" v-if="deleteModal">
+    <transition name="fade">
+      <div v-if="deleteModal">
+        <Teleport to=".modals" >
           <Modal teleport="body" 
             title="Delete Project"
             message="Are you sure you want to delete this project?"
@@ -28,25 +60,24 @@
             <button class="rounded px-4 bg-white border bg-red-700 text-white focus:bg-red-800 border-slate-300 py-1" @click="removeProject(deleteProjects)">Delete</button>
           </Modal>
         </Teleport>
-      </transition>
-      
+      </div>
+    </transition>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted, onUpdated, ref, watchEffect }  from "vue";
-import AlertMessage from "@/components/AlertMessage.vue";
-import TaskList from "@/components/TaskList.vue";
-import useProjects from "@/composables/Projects.ts";
+import { computed, onMounted, ref }  from "vue";
+import useProjects from "@/composables/Projects";
 import SingleProject from "@/components/SingleProject.vue";
 import Loading from "@/components/LoadingComponent.vue";
 import Modal from "@/components/ModalComponent.vue";
-const { getData, deleteProject } = useProjects();
+const { getData, deleteProject, updateStatus } = useProjects();
 const projects = ref([] as Projects[]);
 const isLoading = ref(false);
 const deleteModal = ref(false);
 const updateModal = ref(false);
 const delId = ref();
+let completed = ref(false);
 
 type Projects = {
   id: string,
@@ -63,6 +94,19 @@ const updateDialog = () => {
   return updateModal.value = !updateModal.value;
 }
 
+const completeTask = async (task: any) => {
+  isLoading.value = true
+  await updateStatus(
+    task.id, 
+    task.title,
+    task.description,
+    task.completed
+  )
+  completed.value = true;
+  isLoading.value = false;
+  displayProjects() 
+} 
+
 const displayProjects = async () => {
   isLoading.value = true;
   projects.value = await getData();
@@ -71,7 +115,9 @@ const displayProjects = async () => {
 }
 
 const deleteProjects = (id: any) => {
+  deleteModal.value = true;
   delId.value = id;
+  console.log(deleteModal.value)
 }
 
 const removeProject = async () => {
@@ -94,24 +140,39 @@ onMounted(() => {
 </script>
 
 <style scoped>
-
 h1 {
   color: #314cb6;
 }
+
+.ongoing {
+  min-height: 40vh;
+}
+
 .fade-enter-from {
   opacity: 0;
-} 
+  transform: scale(0.6);
+}
+
+.fade-enter-to {
+  opacity: 1;
+  transform: scale(1);
+}
 
 .fade-enter-active {
-  transition: all .5s ease-in;
-  transform: translateX(30px);
+  transition: all 0.4s ease;
+}
+
+.fade-leave-from {
+  opacity: 1;
+  transform: scale(1);
 }
 
 .fade-leave-to {
   opacity: 0;
+  transform: scale(0.6);
 }
 
 .fade-leave-active {
-  transition: all .5s ease-out;
+  transition: all 0.4s ease;
 }
 </style>
