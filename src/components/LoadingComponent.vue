ab4af0eafd7e42becab4ce48a1fe050debc10e88
= <j.kurtbayaras@gmail.com>
2022-09-17 19:35:28 +0800

    refactor: change loading height

--- src/components/LoadingComponent.vue
+++ src/components/LoadingComponent.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="border shadow py-5 px-10 rounded border ">
-    <div class="animate-pulse flex space-x-4">
+  <div class="border shadow py-3 px-10 rounded border ">
+    <div class="animate-pulse flex space-x-4" >
       <div class="flex-1 space-y-6 py-1">
         <div class="h-2 bg-slate-200 rounded"></div>
         <div class="space-y-3">
