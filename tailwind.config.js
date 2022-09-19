fb59f418e49726c8b0b5869306068e50ed5916de
= <j.kurtbayaras@gmail.com>
2022-09-19 20:17:45 +0800

    feature: add screen media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '375px',
+      // => @media (min-width: 640px) { ... } 
+  
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+  
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+  
+      'xl': '1920',
+      // => @media (min-width: 1280px) { ... }
+    }
   },
   plugins: [],
 }
