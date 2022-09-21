9e0efb74b422dabeabb76ec1f5243dbc40459af1
= <j.kurtbayaras@gmail.com>
2022-09-21 21:17:58 +0800

    feature: change login view and error design

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="flex flex-col">
-    <form @submit.prevent>
-     
-      <h1 class="text-3xl mt-5 mb-5 text-center">Login</h1>
-      <input type="text" class="input mb-5 w-full border-b-2 border-slate-200  py-4 pl-2 pr-3 outline-none focus:outline-hidden focus:border-b-2 focus:border-b-sky-500 focus:transition-all focus:ring-0 sm:text-sm" placeholder="Email" v-model="login.email"/>
-      <input type="password" class="input mb-5 w-full  border-b-2 border-slate-200  py-4 pl-2 pr-3 outline-none focus:outline-hidden focus:border-b-2 focus:border-b-sky-500 focus:transition-all focus:ring-0 sm:text-sm" placeholder="Password" v-model="login.password"/>
-      <div v-if="errMessage" class="pb-2 px-1 text-red-600">
-        {{errMessage}}
-      </div>
-      <button @click="loginUser" class="w-full bg-lime-700 py-3 rounded text-white uppercase font-bold hover:bg-lime-800">Submit</button>
-    </form>
-    <div class="flex items-center py-4">
-      <!-- The left line -->
-      <div class="flex-grow h-px bg-gray-200"></div> 
-
-      <!-- Your text here -->
-      <span class="flex-shrink text-xl text-gray-500 px-4  font-light uppercase">or</span>
-
-      <!-- The right line -->
-      <div class="flex-grow h-px bg-gray-200"></div>
+  <div class="grid grid-cols-12 mt-40 " id="login-wrapper">
+    <div class="col-span-6 bg-gray-200  flex items-center flex-col">
+      <h1 class="text-2xl  mt-10 py-5">JFE Techno Manila</h1>
+      <img src="@/assets/team.svg" class=""/>
     </div>
-    <button @click="signInWithGoogle" class="w-full bg-red-500 py-3 uppercase text-white font-bold text-sm hover:bg-red-700 rounded">Sign in With Google</button>
-  </div>
+    <div class="flex flex-col col-span-6 px-5 py-5">
+      <form @submit.prevent>
+        <h1 class="text-3xl mt-10 mb-20 text-center font-medium">Welcome Back</h1>
+        <div @click="signInWithGoogle" class="flex items-center justify-center w-full shadow-md border-solid border-2 border-gray-100 py-3 uppercase text-blue-800 font-bold text-sm rounded cursor-pointer mb-5">
+          <img src="@/assets/google.svg" />
+          <span class="ml-2">sign in with google</span>
+        </div>
+        <div class="flex items-center mt-5 mb-5">
+          <div class="flex-grow h-px bg-gray-200"></div> 
+          <span class="flex-shrink text-sm text-gray-400 px-4 font-medium uppercase">or login with email</span>
+          <div class="flex-grow h-px bg-gray-200"></div>
+        </div>
+        <input type="text" class="input mb-5 w-full border border-1 border-slate-200  py-4 pl-2 pr-3 outline-none focus:outline-hidden focus:border-b-2 focus:border-slate-500 focus:transition-all focus:ring-0 sm:text-sm" :class="[errorEmail ? 'errorClass' : 'borderClass']" placeholder="Email" v-model="login.email"/>
+        <input type="password" class="input mb-5 w-full border  border-1 border-slate-200  py-4 pl-2 pr-3 outline-none focus:outline-hidden focus:border-b-2 focus:border-slate-500 focus:transition-all focus:ring-0 sm:text-sm" :class="[errorPassword ? 'errorClass' : 'borderClass']" placeholder="Password" v-model="login.password"/>
+        <div v-if="errMessage" class="pb-2 px-1 ">
+          <span class="text-red-600">{{errMessage}}</span>
+        </div>
+        <button @click="loginUser" class="w-full bg-indigo-800 py-3 rounded text-white uppercase font-bold hover:bg-indigo-900">login</button>
+      </form>
+      
+    </div>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -29,7 +33,8 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { getAuth, signInWithEmailAndPassword,GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 const router = useRouter()
-const error = ref(false);
+const errorEmail = ref(false);
+const errorPassword = ref(false);
 const errMessage = ref();
 type SignIn = {
   email: string,
@@ -49,19 +54,43 @@ const loginUser = () => {
     router.push("/")
   })
   .catch((error) => {
-    switch(error.code) {
-      case "auth/invalid-email":
-        errMessage.value = "Invalid Email";
-        break;
-      case "auth/user-not-found":
-        errMessage.value = "No account with that email was found";
-        break;
-      case "auth/wrong-password":
-        errMessage.value = "Incorrect password";
-        break;
-      default:
-        errMessage.value = "Email or password is incorrect"
-        break;
+    if(login.value.email !== "" && login.value.password !== "") {
+      switch(error.code) {
+        case "auth/invalid-email":
+          errMessage.value = "Please enter a valid email address";
+          errorEmail.value = true;
+          break;
+        case "auth/user-not-found":
+          errMessage.value = "No account with that email was found";
+          errorEmail.value = true;
+          break;
+        case "auth/wrong-password":
+          errMessage.value = "Password is incorrect";
+          errorPassword.value = true
+          break;
+        default:
+          errMessage.value = "Email or password is incorrect"
+          errorEmail.value = true;
+          errorPassword.value = true
+          break;
+      }
+    } else if (login.value.email !== "" && login.value.password === "") {
+      switch(error.code) {
+        case "auth/wrong-password":
+          errMessage.value = "Password is incorrect";
+          errorPassword.value = true;
+          errorEmail.value = false;
+          break;
+        default:
+          errMessage.value = "Password is incorrect"
+          errorPassword.value = true;
+          errorEmail.value = false;
+          break;
+      }
+    } else {
+      errMessage.value = "Email or Password should not be empty"
+      errorEmail.value = true;
+      errorPassword.value = true;
     }
   })
 }
@@ -81,6 +110,20 @@ const signInWithGoogle = () => {
 </script>
 
 <style scoped>
+#login-wrapper {
+  border: 1px solid lightgray;
+  min-height: 55vh;
+}
 
+h1 {
+  font-family: 'Roboto';
+}
 
+.errorClass {
+  border: 1px solid #DC2626
+}
+
+.borderClass {
+  border: 1px solid #e5e7eb
+}
 </style>
