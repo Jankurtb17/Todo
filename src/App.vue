14c924a5d277fdf0f43114f95d368becd4792cfb
= <j.kurtbayaras@gmail.com>
2022-09-21 21:18:44 +0800

    feature: hide navbar in login

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar />  
+  <Navbar v-if="!$route.meta.hideNavbar"/>  
   <div class="app">
     <div class="container mx-auto">
       <router-view />
@@ -10,3 +10,10 @@
 <script lang="ts" setup>
 import Navbar from "@/components/NavbarComponent.vue"
 </script>
+
+<style scoped>
+body {
+  font-family: 'Roboto', sans-serif;
+  font-family: 'Roboto Condensed', sans-serif;
+}
+</style>
