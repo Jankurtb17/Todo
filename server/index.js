b129b8069dc3787985b1f1a1b46d39fc8f491f81
= <j.kurtbayaras@gmail.com>
2022-10-09 15:27:21 +0800

    fix: add slash in server/index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use('/api/posts', posts);
 
 if(process.env.NODE_ENV === 'production') {
   // Statice folder
-  app.use(express.static(__dirname+'/public'))
+  app.use(express.static(__dirname+'/public/'))
   // Handle SPA something
   app.get(/.*/, (req, res) => res.sendFile(__dirname+'/public/index.html'));
 }
