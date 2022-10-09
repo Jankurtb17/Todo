c51f85149e2bc1dbffebb58949d7a7522fe36414
= <j.kurtbayaras@gmail.com>
2022-10-09 21:15:13 +0800

    fix: change helment to helmet

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const helment = require('helmet')
+const helmet = require('helmet')
 const app = express();
 
 //Middleware
