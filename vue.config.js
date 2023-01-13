acca18a0a228212a2e6588333e6caeaeb8875bc2
= <j.kurtbayaras@gmail.com>
2023-01-13 23:39:48 +0800

    refactor: comment publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 const path = require('path')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '../dist/'
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '../dist/'
+  //   : '/',
   outputDir: path.resolve(__dirname, '../dist'),
   devServer: {
     proxy: {
